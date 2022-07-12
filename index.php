b10c8cb7ce06e1351de23baa303734e31fe54917
Mel-SON <omelyanchenko.semen@yandex.ru>
2022-07-12 20:20:38 +0300

    Смена расположения гита для написания плагина

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
 /**
- * Index template file.
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
  *
- * @package Understrapchild\Template
+ * @package WordPress
  */
 
-get_header();
-the_post();
-the_content();
-get_footer();
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
+
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
---
+++ wp-content/themes/understrap-child-1.1.0/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Index template file.
+ *
+ * @package Understrapchild\Template
+ */
+
+get_header();
+the_post();
+the_content();
+get_footer();
