0459e590134609fbe78eaa3a6c1dc4a2ff87a66b
Mel-SON <omelyanchenko.semen@yandex.ru>
2022-07-12 18:26:12 +0300

    Меняет+запускает свайпер слайдер

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,16 @@
-var swiper = new Swiper(".mySwiper", {
+var gallery_swiper = new Swiper(".gallery_thumb_swiper", {
     loop: true,
     spaceBetween: 10,
     slidesPerView: 4,
-    freeMode: true,
-    watchSlidesProgress: true,
 });
-var swiper2 = new Swiper(".mySwiper2", {
+var gallery_swiper2 = new Swiper(".gallery_swiper", {
     loop: true,
     spaceBetween: 10,
     navigation: {
-      nextEl: ".swiper-button-next",
+      nextEl: ".swiper-button-next",    
       prevEl: ".swiper-button-prev",
     },
     thumbs: {
-      swiper: swiper,
+      swiper: gallery_swiper,
     },
 });
