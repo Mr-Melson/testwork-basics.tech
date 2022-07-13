b8498ae48ac6814bd8f230e4b55fbc9b291ce2e8
Mel-SON <omelyanchenko.semen@yandex.ru>
2022-07-13 17:29:07 +0300

    Добавляет измерения

--- wp-content/themes/understrap-child-1.1.0/template-parts/common-parts/realty-info.php
+++ wp-content/themes/understrap-child-1.1.0/template-parts/common-parts/realty-info.php
@@ -1,12 +1,12 @@
 <ul class="list-group list-group-flush">
     <?php if( $square = get_field( 'square' ) ): ?>
-        <li class="list-group-item">Площадь: <?php echo $square ?></li>
+        <li class="list-group-item">Площадь: <?php echo $square ?> м²</li>
     <?php endif; ?>
     <?php if( $price = get_field( 'price' ) ): ?>
-        <li class="list-group-item">Стоимость: <?php echo number_format( $price, 0, '', ' '); ?></li>
+        <li class="list-group-item">Стоимость: <?php echo number_format( $price, 0, '', ' '); ?> ₽</li>
     <?php endif; ?>
     <?php if( $living_space = get_field( 'living_space' ) ): ?>
-        <li class="list-group-item">Жилая площадь: <?php echo $living_space ?></li>
+        <li class="list-group-item">Жилая площадь: <?php echo $living_space ?> м²</li>
     <?php endif; ?>
     <?php if( $floor = get_field( 'floor' ) ): ?>
         <li class="list-group-item">Этаж: <?php echo $floor ?></li>
