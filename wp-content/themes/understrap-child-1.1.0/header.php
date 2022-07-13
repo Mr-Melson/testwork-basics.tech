aa9e230f2d1b0b4b7154716cd308a56c94d2db5c
Mel-SON <omelyanchenko.semen@yandex.ru>
2022-07-13 17:19:13 +0300

    Добавляет плейсхолдер для поиска

--- wp-content/themes/understrap-child-1.1.0/header.php
+++ wp-content/themes/understrap-child-1.1.0/header.php
@@ -23,7 +23,7 @@
 					<div id="search">
 						<form role="search" method="get" id="searchform" class="row" action="<?php echo home_url( '/' ) ?>" >
 							<div class="col-sm-8">
-								<input type="text" value="<?php echo get_search_query() ?>" name="s" id="s" class="form-control"/>
+								<input type="text" value="<?php echo get_search_query() ?>" name="s" id="s" class="form-control" placeholder="Поиск..."/>
 							</div>
 							<div class="col-sm">
 								<input type="submit" id="searchsubmit" value="Найти" class="btn btn-dark"/>
